f1d4ce1fdec535b3a9330f04bb708443b874e67b
admin <promitay@gmail.com>
2023-12-26 22:44:34 +0300

    objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks) {
+        this.marks.push(...marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.marks || !this.marks.length) {
+        return 0;
+    }
+    return this.marks.reduce((acc, item, idx, arr) => {
+        if (idx === arr.length - 1) {
+            return (acc + item) / arr.length;
+        } else {
+            return acc + item;
+        }
+    })
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
 }
